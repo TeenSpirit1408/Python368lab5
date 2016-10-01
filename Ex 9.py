fc0e7f4ba06e13a0976ffd7e3714b3202618836a
TeenSpirit1408 <Mentalist26021999@gmail.com>
2016-10-01 23:50:18 +0300

    Ex 9

--- Ex 9.py
+++ Ex 9.py
@@ -1,7 +1,14 @@
 f = open('/home/user/Рабочий стол/input.txt', 'r')
 s = f.read()
-print(s)
-line = list(map(int, f.readline().split()))
-print(line)
+max = 0
+A = list(map(int, s.split()))
+n = A.pop(0)
+k = A.pop(-1)
+for i in range(n):
+    if sum(A[i:k+2]) > max:
+        max = sum(A[i:k])
+        k += 1
+        print(max)
+print(max)
 
 
