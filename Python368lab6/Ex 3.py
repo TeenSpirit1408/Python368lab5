a16e6c0c55c35f17f9d9c6bf28a0e25213ebf501
TeenSpirit1408 <Mentalist26021999@gmail.com>
2016-10-09 20:54:57 +0300

    Ex 3

--- Python368lab6/Ex 3.py
+++ Python368lab6/Ex 3.py
@@ -1,5 +1,10 @@
 __author__ = 'student'
 import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+file_input = open('/home/user/Рабочий стол/img.txt', 'r')
+
 
 #1
 def get_percentile(values, bucket_number):
@@ -16,31 +21,61 @@ def get_percentile_number(value, percentiles):
     max = 0
     imax = 0
     for i in range(len(percentiles)):
-        if max <= percentiles[i] <= value:
+        if (max <= percentiles[i]) &  (percentiles[i] <= value):
             max = percentiles[i]
             imax = i
     return imax
 
-
-#3
-def value_equalization(value, percentiles):
+#3, 4
+def value_equalization(value, percentiles, add_random = False):
     idx = get_percentile_number(value, percentiles)
-    step = 1/len(percentiles)
-    new_value = idx*step
+    step = 1 / len(percentiles)
+    if add_random == True:
+        random_noise = random.uniform(0, (idx+1)*step  - (idx)*step)
+        new_value = idx*step + random_noise
+    else:
+        new_value = idx*step
     return new_value
 
+#5
+def values_equalization(values, percentiles, add_random=False):
+    A = []
+    for i in range(len(values)):
+        A.append(value_equalization(values[i], percentiles, add_random))
+    return(A)
 
+#6
+k = 0
+data = []
+for i in range(200):
+    data.append([])
+    for j in range(267):
+        data[i].append(k)
+data1 = file_input.read().split()
+for i in range(200):
+    for j in range(267):
+        data[i][j] = float(data1[k])
+        k += 1
+data = np.array(data)
 
+#7
+plt.imshow(data, cmap=plt.get_cmap('gray'))
+plt.show()
 
+#8
+data = data.flatten()
+plt.hist(data, bins=11)
+plt.show()
 
+#9
 
-A = [3, 4, 1, 2, 5, 6, 7, 8, 9, 10]
-n = int(input())
-B = get_percentile(A, n)
-print(B)
+B = get_percentile(data, 5)
+b = value_equalization(data, B, add_random=True)
+data = values_equalization(data, B, add_random=True)
+print(data)
 
-k = float(input())
-print(get_percentile_number(k, B))
-
-l = float(input())
-print(value_equalization(l, B))
+plt.imshow(data, cmap=plt.get_cmap('gray'))
+plt.show()
+data = data.flatten()
+plt.hist(data, bins=11)
+plt.show()
